d62229ad474eed2466c44e16ace327795b783ba7
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-03-25 18:35:21 +0200

    Remove bad comma

--- launch/recorder.py
+++ launch/recorder.py
@@ -36,7 +36,7 @@ def generate_launch_description():
         package="tf2_ros",
         executable="static_transform_publisher",
         arguments=["0", "0", "0", "0", "0", "0", "map", "laser"],
-    ),
+    )
     
 
     return LaunchDescription([
