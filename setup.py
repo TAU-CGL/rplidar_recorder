e33cf17affcd84f866d7b19d2e3fb7ece1a23fd2
Michael Bilevich <mickelbil84@gmail.com>
2025-03-25 15:35:11 +0000

    Regenerate ros package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
-package_name = 'rplidar-recorder'
+package_name = 'rplidar_recorder'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,14 +13,13 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='mickelbil84',
+    maintainer='ubuntu',
     maintainer_email='mickelbil84@gmail.com',
-    description='ROS2 package for continous recording of 2D LiDAR data',
-    license='MIT',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'led_blinker = rplidar_recorder.led_blinker:main',
         ],
     },
 )
