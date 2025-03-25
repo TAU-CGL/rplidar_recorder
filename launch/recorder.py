f9ca014e4ba214a90c1101fd8ebc99c738cac0a5
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-03-25 18:14:49 +0200

    Update launch file to be sequential

--- launch/recorder.py
+++ launch/recorder.py
@@ -1,4 +1,7 @@
 from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.events.process import ProcessStarted
+from launch.event_handlers.on_process_start import OnProcessStart
 from launch_ros.actions import Node
 
 from gpiozero import LED
@@ -6,11 +9,12 @@ LED_PIN = 16 # Red
 
 def generate_launch_description():
     LED(LED_PIN).on()
-    return LaunchDescription([
-        Node(
+
+    rplidar_ros = Node(
             package='rplidar_ros',
             executable='rplidar_node',
             name='rplidar_node',
+            exec_name='rplidar_node',
             parameters=[{'channel_type':"serial",
                         'serial_port': "/dev/ttyUSB0",
                         'serial_baudrate': "460800",
@@ -18,11 +22,16 @@ def generate_launch_description():
                         'inverted': "false",
                         'angle_compensate': "true",
                         'scan_mode': "Standard"}],
-            output='screen'),
-        Node(
+            output='screen')
+    led_blinker = Node(
             package='rplidar_recorder',
             executable='led_blinker',
             name='led_blinker',
             output='screen'
-        ),
+        )
+    
+
+    return LaunchDescription([
+        RegisterEventHandler(event_handler=OnProcessStart(target_action=rplidar_ros, on_start=led_blinker)),
+        rplidar_ros,
     ])
