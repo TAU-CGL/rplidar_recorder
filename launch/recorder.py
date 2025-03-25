89850d363265e9c322a8ad7d02b9a116acce0b62
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-03-25 18:16:34 +0200

    Update rplidar node to be required

---
+++ launch/recorder.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.events.process import ProcessStarted
+from launch.event_handlers.on_process_start import OnProcessStart
+from launch_ros.actions import Node
+
+from gpiozero import LED
+LED_PIN = 16 # Red
+
+def generate_launch_description():
+    LED(LED_PIN).on()
+
+    rplidar_ros = Node(
+            required=True,
+            package='rplidar_ros',
+            executable='rplidar_node',
+            name='rplidar_node',
+            exec_name='rplidar_node',
+            parameters=[{'channel_type':"serial",
+                        'serial_port': "/dev/ttyUSB0",
+                        'serial_baudrate': "460800",
+                        'frame_id': "laser",
+                        'inverted': "false",
+                        'angle_compensate': "true",
+                        'scan_mode': "Standard"}],
+            output='screen')
+    led_blinker = Node(
+            package='rplidar_recorder',
+            executable='led_blinker',
+            name='led_blinker',
+            output='screen'
+        )
+    
+
+    return LaunchDescription([
+        RegisterEventHandler(event_handler=OnProcessStart(target_action=rplidar_ros, on_start=led_blinker)),
+        rplidar_ros,
+    ])
