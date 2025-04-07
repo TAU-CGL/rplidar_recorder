72486f589f64083925f4022a18f993981ddfb4ae
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-04-07 16:27:45 +0300

    Update launch.bash

--- launch.bash
+++ launch.bash
@@ -2,4 +2,4 @@
 sudo chmod 777 /dev/ttyUSB0
 source /opt/ros/humble/setup.bash
 source /home/ubuntu/ros_ws/install/setup.bash
-ros2 launch rplidar_recorder recorder.py
+ros2 launch /home/ubuntu/ros_ws/src/rplidar_recorder/launch/recorder.py
