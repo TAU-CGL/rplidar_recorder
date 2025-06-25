57e6725b8427d7c226cc1a2f2427d8808608c258
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-06-25 17:20:27 +0300

    Add init name script

--- launch.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo chmod 777 /dev/ttyUSB0
-source /opt/ros/humble/setup.bash
-source /home/ubuntu/ros_ws/install/setup.bash
-ros2 launch /home/ubuntu/ros_ws/src/rplidar_recorder/launch/recorder.py &
--- server/index/urls.py
+++ server/index/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('api/new-contraption', new_contraption, name='new_contraption'),
+    path('api/contraption/register', new_contraption, name='new_contraption'),
 
     path('teapot', teapot, name='teapot'),
 ]
