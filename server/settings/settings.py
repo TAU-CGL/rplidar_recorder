1a401d3b8cb7a572d89647c43049f503817cdb82
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-06-25 16:52:16 +0300

    Allow iframe of admin page

--- server/settings/settings.py
+++ server/settings/settings.py
@@ -132,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Allow iframe of admin page
+X_FRAME_OPTIONS = 'SAMEORIGIN'
