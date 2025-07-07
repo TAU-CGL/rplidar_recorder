197ce0db403011c114999997b0d0fd34f7036dd5
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-07-07 19:53:31 +0300

    Add contraption admin ordering

--- server/index/admin.py
+++ server/index/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Contraption, LaserScan
 
 # Register your models here.
-admin.site.register(Contraption)
+class ContraptionAdmin(admin.ModelAdmin):
+    ordering = ('nickname',)
+admin.site.register(Contraption, ContraptionAdmin)
+
+
 admin.site.register(LaserScan)
