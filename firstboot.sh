6b04bfb4bc0ab8275264f2c324572aa3bee78b16
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-07-07 14:10:59 +0300

    Update boot script

--- firstboot.sh
+++ firstboot.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get update -y
+
+
+
+##################################################
+# Initial boot - make sure apt is correctly updated
+# For the Ubuntu image for respberry pi, 
+# you sometimes may need to run update twice.
+##################################################
+sudo apt-get update
+sudo apt-get update
+
+#######################
+# Install ROS2 Humble
+#######################
 sudo apt install software-properties-common -y
 sudo add-apt-repository universe -y
 sudo apt update && sudo apt install curl -y
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
 sudo dpkg -i /tmp/ros2-apt-source.deb
-sudo apt update -y
+sudo apt update
 sudo apt upgrade -y
-sudo apt install ros-humble-ros-base
-sudo apt install ros-dev-tools
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+sudo apt install ros-humble-ros-base -y
+sudo apt install ros-dev-tools -y
+
+##############################################
+# Install RPLIDAR ROS2 & recorder packages
+# If this script is run with sudo, this will
+# install everything in `/root/ros_ws`
+##############################################
+source /opt/ros/humble/setup.bash
 cd ~ && mkdir ros_ws && mkdir ros_ws/src && cd ros_ws/src
 git clone https://github.com/Slamtec/rplidar_ros.git
-cd rplidar_ros && gco ros2 && cd ..
+cd rplidar_ros && git checkout ros2 && cd ..
 git clone https://github.com/TAU-CGL/rplidar_recorder.git
 cd ~/ros_ws && colcon build
-echo "source ~/ros_ws/install/setup.bash" >> ~/.bashrc
+
+################################
+# Install misc dependencies
+################################
 sudo apt-get install python3-gpiozero -y
-sudo chmod a+x /home/ubuntu/ros_ws/src/rplidar_recorder/scripts/launch.bash
-line=@reboot /home/ubuntu/ros_ws/src/rplidar_recorder/scripts/launch.bash
+
+################################
+# Add boot script to crontab
+################################
+sudo chmod a+x /root/ros_ws/src/rplidar_recorder/scripts/launch.bash
+line=@reboot /root/ros_ws/src/rplidar_recorder/scripts/launch.bash
 (sudo crontab -l; echo "$line") | sudo crontab -
--- scripts/launch.bash
+++ scripts/launch.bash
@@ -3,14 +3,14 @@
 # On first boot - make sure the contraption is registered in the system
 # and save its name to ~/.contraption_uuid
 export SERVER_URL="<server_url>"
-python3 /home/ubuntu/ros_ws/src/rplidar_recorder/scripts/init_name.py
+python3 /root/ros_ws/src/rplidar_recorder/scripts/init_name.py
 
 # Setup USB permissions for the RPLIDAR device
 sudo chmod 777 /dev/ttyUSB0
 
 # Source (prepare) the ROS2 environment - global humble install and local ros workspace
 source /opt/ros/humble/setup.bash
-source /home/ubuntu/ros_ws/install/setup.bash
+source /root/ros_ws/install/setup.bash
 
 # Start the ROS2 nodes (single launch file, many nodes)
-ros2 launch /home/ubuntu/ros_ws/src/rplidar_recorder/launch/recorder.py &
+ros2 launch /root/ros_ws/src/rplidar_recorder/launch/recorder.py &
