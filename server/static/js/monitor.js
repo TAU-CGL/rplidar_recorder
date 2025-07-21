88673949e50385ff046e4c53ea19aa01d794bb22
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-07-21 15:41:05 +0300

    Change to scatter plot

--- server/static/js/monitor.js
+++ server/static/js/monitor.js
@@ -48,23 +48,18 @@ function update_current_scan() {
 function update_current_scan_viewport() {
     monitorNoSelection.style.display = 'none';
     if (activeScans.chart != null) activeScans.chart.destroy();
+
+    let data = {
+        datasets: [ {
+            label: 'scan',
+            data: [[0,0], [1,1], 2,2]    
+        }]
+    }
+
     activeScans.chart = new Chart(monitorViewport, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-    }})
+        type: 'scatter',
+        data: data,
+        options: {}})
 }
 
 function on_prev_next_scan_click(dir, event) {
