fdef50a470fd627189baffe6062998eff5c34728
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-07-28 13:40:21 +0300

    Refactor misc page to sparate js file

---
+++ server/static/js/miscellaneous.js
@@ -0,0 +1,35 @@
+function sendNewContraption() {
+    fetch('/api/contraption/register')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('new-contraption-result').value = data;
+        })
+        .catch(error => {
+            document.getElementById('new-contraption-result').value = 'Error: ' + error;
+        });
+}
+
+function deleteAllScans() {
+    fetch('/api/contraption/scan/delete', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        alert('All scans deleted successfully');
+    })
+    .catch(error => {
+        alert('Error deleting scans: ' + error);
+    });
+}
+
+function fetch_all_recent_scans() {
+    
+}
